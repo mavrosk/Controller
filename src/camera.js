0b3cba39dbbc5df910ce12aab95eb5b948139c18
Nikos <ntzamos@gmail.com>
2021-09-07 15:25:13 +0300

    controls

--- src/camera.js
+++ src/camera.js
@@ -1,55 +1,55 @@
 var path = require('path');
 var conf = require('./config.json');
-var fs = require("fs");
-
+var v4l2camera = require("@hypersolution/v4l2camera");
 var NodeWebcam = require('node-webcam');
 Webcam = NodeWebcam.create({});
 
-var v4l2camera = require("@hypersolution/v4l2camera");
  
 
 var availableCameras = [];
 
-async function takePicture(device, focus=300, exposure=156) {
+function getAvailableCameras(cb) {
+  Webcam.list((devices) => {
+    // devices = devices.filter((a)=>a.includes('4K'))
+    availableCameras = devices;
+    cb(devices);
+  })
+}
+
+async function takePicture(device, options, cb) {
+  
   var cam = new v4l2camera.Camera(device);
 
   if (cam.configGet().formatName !== "MJPG") {
     console.log("NOTICE: MJPG camera required");
     return;
   }
-  console.log(Object.keys(cam.controls))
+  // console.log(Object.keys(cam.controls))
 
   focusAuto = cam.controls['Focus, Auto'];
   focusAbsolute = cam.controls['Focus (absolute)'];
   exposureAuto = cam.controls['Exposure, Auto'];
   exposureAbsolute = cam.controls['Exposure (Absolute)'];
 
+  cam.start();
   cam.controlSet(focusAuto.id, 0)
-  cam.controlSet(focusAbsolute.id, focus)
+  cam.controlSet(focusAbsolute.id, options.focus)
 
   cam.controlSet(exposureAuto.id, 1)
-  cam.controlSet(exposureAbsolute.id, exposure)
+  cam.controlSet(exposureAbsolute.id, options.exposure)
   
-  cam.start();
-  await new Promise(resolve => setTimeout(resolve, 300));
+  await new Promise(resolve => setTimeout(resolve, 500));
   cam.capture((success) => {
 
     let frame = cam.frameRaw();
     cam.stop(()=>{
-      fs.createWriteStream(conf.camera.picturesFolder + "/camera" + device.slice(device.length - 1) + ".jpg").end(frame);
+      cb(frame);
+      // fs.createWriteStream(conf.camera.picturesFolder + "/camera" + device.slice(device.length - 1) + ".jpg").end(frame);
     });
   });
 }
 
 
-function getAvailableCameras(cb) {
-  Webcam.list((devices) => {
-    // devices = devices.filter((a)=>a.includes('4K'))
-    availableCameras = devices;
-    
-    cb(devices);
-  })
-}
 
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
 const camera = require('./camera')
+var conf = require('./config.json');
 // const controller = require('./controller')
+var fs = require("fs");
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-var devices = [];
-camera.getAvailableCameras((d)=> {
-     console.log(d);
-     devices = d;
-});
 
 
 // controller.init(()=> {
 
 // });
-
-
 
 rl.setPrompt('PORT> ');
 rl.prompt();
@@ -33,7 +28,20 @@ rl.on('line', async function(line) {
         await new Promise(resolve => setTimeout(resolve, 5000));
         controller.write('q')
     } else {
-        camera.takePicture(devices[0], 300, parseInt(line)*1000);
+        var options = line.split('.')
+        var focus = parseInt(options[0]);
+        var exposure = parseInt(options[1]);
+        camera.getAvailableCameras((devices)=> {
+            console.log(devices);
+            for(var i=0;i<devices.length;i+=2) {
+                device = devices[i]
+                deviceId = parseInt(device.slice(device.length - 1))/2;
+                camera.takePicture(device, { focus, exposure: exposure*1000 }, (result) => {
+                    fs.createWriteStream(conf.camera.picturesFolder + "/camera" + deviceId + "_f" + focus + "_e" + exposure + ".jpg").end(result);
+                });
+            }
+        });
+        
     }
     
     rl.prompt();
